9b66a86c9ec3433a49cee34e7657f655b6a95e8b
Liza Arora <liza.arora@outlook.com>
2022-05-16 01:57:32 +1000

    Lab complete

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code goes here
+// document.addEventListener("DOMContentLoaded", function() {
+//     console.log("The DOM has loaded");
+//   });
+
+//   console.log(
+//     "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+//   );
+
+
+  document.addEventListener("DOMContentLoaded", function replaceText() {
+      let text = document.getElementById("text");
+
+      text.innerHTML = "This is really cool!";
+
+  });
+  
